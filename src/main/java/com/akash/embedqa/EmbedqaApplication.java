41c44abc6754cafd0aa7ffc65289433c155a0781
Akash <akash.bhuiyan@gmail.com>
2025-10-29 11:25:40 +0100

    Initial project setup added

---
+++ src/main/java/com/akash/embedqa/EmbedqaApplication.java
@@ -0,0 +1,13 @@
+package com.akash.embedqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akash/embedqa/EmbedqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.embedqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
